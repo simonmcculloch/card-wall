95cea5d12fdd56c8c248ba7924d2e7c2a9e88ad9
Simon McCulloch <sdm@DEV-006-ASUS.Insite.local>
2012-11-15 16:22:55 +0800

    Add cookieParse to app.js

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.configure(function(){
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
   app.use(express.session({ secret: 'blah' }));
   app.use(express.methodOverride());
   app.use(app.router);
