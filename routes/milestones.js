ef414288f680227966c9623847a3611fc5cdf75c
sdm <smcculloch@brighttechnologies.com.au>
2012-11-20 16:56:14 +0800

    Remove unused require

--- routes/milestones.js
+++ routes/milestones.js
@@ -1,12 +1,11 @@
 
 exports.index = function(req, res) {
 	
-	var url = require('url')
-	, assembla = require('../assembla');
+	var assembla = require('../assembla');
 
 	var milestoneId = req.params.id;
-	console.log('Requesting Milestone ' + milestoneId);
 	
+	console.log('Requesting Milestone ' + milestoneId);	
 	
 	assembla.get(req, res, 'milestones/' + milestoneId + '.json', req.params.accessToken);
 
