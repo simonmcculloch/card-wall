8bf712cef6c5ac57e4d9d9d9e0411238dc9acfe3
sdm <smcculloch@brighttechnologies.com.au>
2012-12-13 10:51:57 +0800

    Tidy up tickets route to stop polluting the global namespace

--- routes/tickets.js
+++ routes/tickets.js
@@ -1,25 +1,25 @@
-
-exports.index = function(req, res) {
-
-	var assembla = require('../assembla');
-
-	var milestoneId = req.params.milestoneId;
-
-	console.log('Requesting tickets for Milestone ' + milestoneId);
-	
-	assembla.get(req, res, 'tickets/milestone/' + milestoneId + '.json?per_page=1000');
-
-};
-
-exports.statuses = function(req, res) {
+(function() {
 
 	var assembla = require('../assembla');
 
-	var milestoneId = req.params.milestoneId;
+	exports.index = function(req, res) {
 
-	console.log('Requesting ticket statues');
-	
-	assembla.get(req, res, 'tickets/statuses.json');
+		var milestoneId = req.params.milestoneId;
 
-};
+		console.log('Requesting tickets for Milestone ' + milestoneId);
+		
+		assembla.get(req, res, 'tickets/milestone/' + milestoneId + '.json?per_page=1000');
+
+	};
+
+	exports.statuses = function(req, res) {
+
+		var milestoneId = req.params.milestoneId;
+
+		console.log('Requesting ticket statues');
+		
+		assembla.get(req, res, 'tickets/statuses.json');
+
+	};
+})()
 
